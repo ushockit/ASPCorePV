0417c6a2cdc9f4d1e3df3d3dcb590fe55b1bcf5a
Виталий Любимов <us@itstep1.dp.ua>
2021-08-17 19:42:19 +0300

    Lesson 15

---
+++ Lesson_15/WebApplication1/WebApplication1/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+    }
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var ctx = Context;
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message);
+        }
+    }
+}
